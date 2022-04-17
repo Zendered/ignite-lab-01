ac0ac50632fc9557f186cfa2000d7faad579af61
Luiz <luizz.ggr@gmail.com>
2022-04-17 18:30:28 -0300

    feat: testing authentication controller

--- purchases/src/http/auth.controller.ts
+++ purchases/src/http/auth.controller.ts
@@ -1,11 +1,14 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
+import { PrismaService } from 'src/database/prisma/prisma.service';
 import { AuthorizationGuard } from 'src/http/auth/authorization.guard';
 
 @Controller()
 export class AuthController {
+  constructor(private prisma: PrismaService) {}
+
   @Get()
   @UseGuards(AuthorizationGuard)
   hello() {
-    return 'hello';
+    return 'Hello World';
   }
 }
