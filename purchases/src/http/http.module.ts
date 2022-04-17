0f027611d4fcb66a7adbeb3c35af59cfe62730d3
Luiz <luizz.ggr@gmail.com>
2022-04-17 17:28:50 -0300

    feat: add authoController

--- purchases/src/http/http.module.ts
+++ purchases/src/http/http.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { AuthController } from './auth.controller';
 
 @Module({
   imports: [ConfigModule.forRoot()],
+  controllers: [AuthController],
 })
 export class HttpModule {}
