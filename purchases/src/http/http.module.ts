9e83aaae84eb75af4d32c82cb0f0fbf011808a6f
Luiz <luizz.ggr@gmail.com>
2022-04-18 15:55:15 -0300

    feat: adding purchaseController and purschaseService to http module

--- purchases/src/http/http.module.ts
+++ purchases/src/http/http.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from 'src/database/database.module';
 import { ProductsService } from 'src/services/products.service';
+import { PurchasesService } from 'src/services/purchases.service';
 import { ProductsController } from './auth/controllers/products.controller';
+import { PurchasesController } from './auth/controllers/purchases.controller';
 
 @Module({
   imports: [ConfigModule.forRoot(), DatabaseModule],
-  controllers: [ProductsController],
-  providers: [ProductsService],
+  controllers: [ProductsController, PurchasesController],
+  providers: [ProductsService, PurchasesService],
 })
 export class HttpModule {}
