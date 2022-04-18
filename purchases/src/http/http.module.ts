407deda1edc5b5ce7a149b70f190d2b3b2ad69ba
Luiz <luizz.ggr@gmail.com>
2022-04-17 22:42:22 -0300

    feat: add products service to http provider

--- purchases/src/http/http.module.ts
+++ purchases/src/http/http.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from 'src/database/database.module';
+import { ProductsService } from 'src/services/products.service';
 import { ProductsController } from './auth/controllers/products.controller';
 
 @Module({
   imports: [ConfigModule.forRoot(), DatabaseModule],
   controllers: [ProductsController],
+  providers: [ProductsService],
 })
 export class HttpModule {}
