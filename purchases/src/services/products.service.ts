f1d367c54e89a25bbd769923f3497956f1f1b84c
Luiz <luizz.ggr@gmail.com>
2022-04-17 22:43:31 -0300

    feat: list all products, and create products

---
+++ purchases/src/services/products.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/database/prisma/prisma.service';
+import slugify from 'slugify';
+import { ICreateProductsDTO } from 'src/interfaces/dto/create-products-dto';
+
+@Injectable()
+export class ProductsService {
+  constructor(private prisma: PrismaService) {}
+
+  listAllProducts() {
+    return this.prisma.product.findMany();
+  }
+
+  async createProducts({ title }: ICreateProductsDTO) {
+    const slug = slugify(title, { lower: true });
+    const productWithSameSlug = await this.prisma.product.findUnique({
+      where: { slug },
+    });
+
+    if (productWithSameSlug) {
+      throw new Error('Another product with the same slug exists!');
+    }
+
+    await this.prisma.product.create({
+      data: {
+        title,
+        slug,
+      },
+    });
+  }
+}
