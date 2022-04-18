9aace89ea335e440a54099a2cfbf6a8b27de09cf
Luiz <luizz.ggr@gmail.com>
2022-04-18 17:38:21 -0300

    feat: create a purchase if customer has a valid id

--- purchases/src/http/auth/controllers/purchases.controller.ts
+++ purchases/src/http/auth/controllers/purchases.controller.ts
@@ -1,26 +1,52 @@
-import { Controller, Get, Param, UseGuards } from '@nestjs/common';
+import { Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
 import { Purchase } from '@prisma/client';
+import { AuthUserDTO } from 'src/interfaces/dto/auth-user-dto';
+import { CustomersService } from 'src/services/curstomers.service';
 import { ProductsService } from 'src/services/products.service';
 // import { AuthorizationGuard } from 'src/http/auth/authorization.guard';
 import { PurchasesService } from 'src/services/purchases.service';
 import { AuthorizationGuard } from '../authorization.guard';
+import { CurrentUser } from '../current-user';
 
 @Controller('api/v1')
 export class PurchasesController {
   constructor(
-    private PurchasesService: PurchasesService,
+    private purchasesService: PurchasesService,
     private productsService: ProductsService,
+    private customersService: CustomersService,
   ) {}
 
   @Get('purchases')
   @UseGuards(AuthorizationGuard)
   products(): Promise<Purchase[]> {
-    return this.PurchasesService.listAllPurchases();
+    return this.purchasesService.listAllPurchases();
   }
 
   @Get(':productId')
   @UseGuards(AuthorizationGuard)
   purchases(@Param('productId') productId: string) {
     return this.productsService.getProductById(productId);
+  }
+
+  @Post(':productId')
+  @UseGuards(AuthorizationGuard)
+  async createPurchase(
+    @Param('productId') productId,
+    @CurrentUser() user: AuthUserDTO,
+  ) {
+    let customer = await this.customersService.getCustomerByAuthUserId(
+      user.sub,
+    );
+
+    if (!customer) {
+      customer = await this.customersService.createCustomer({
+        authUserId: user.sub,
+      });
+    }
+
+    return this.purchasesService.createPurchase({
+      productId,
+      customerId: customer.id,
+    });
   }
 }
