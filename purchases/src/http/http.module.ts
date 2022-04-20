515c19e27267383c6a386182f3ccba0502e33e25
Luiz <luizz.ggr@gmail.com>
2022-04-20 14:28:12 -0300

    feat: adding customer controller to http module

--- purchases/src/http/http.module.ts
+++ purchases/src/http/http.module.ts
@@ -1,15 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from 'src/database/database.module';
-import { CustomersService } from 'src/services/curstomers.service';
+import { CustomersService } from 'src/services/customer.service';
 import { ProductsService } from 'src/services/products.service';
 import { PurchasesService } from 'src/services/purchases.service';
+import { CustomersController } from './auth/controllers/customers.controller';
 import { ProductsController } from './auth/controllers/products.controller';
 import { PurchasesController } from './auth/controllers/purchases.controller';
 
 @Module({
   imports: [ConfigModule.forRoot(), DatabaseModule],
-  controllers: [ProductsController, PurchasesController],
+  controllers: [CustomersController, ProductsController, PurchasesController],
   providers: [ProductsService, PurchasesService, CustomersService],
 })
 export class HttpModule {}
