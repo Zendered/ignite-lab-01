076a85c2fd2b88841576ee2a7505347882024b43
Luiz <luizz.ggr@gmail.com>
2022-04-20 14:32:39 -0300

    feat: user needs to be authenticated to create a product

--- purchases/src/http/auth/controllers/products.controller.ts
+++ purchases/src/http/auth/controllers/products.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { Product } from 'src/entities/product.entity';
-// import { AuthorizationGuard } from 'src/http/auth/authorization.guard';
+import { AuthorizationGuard } from 'src/http/auth/authorization.guard';
 import { ProductsService } from 'src/services/products.service';
 import { ICreateProductsDTO } from 'src/interfaces/dto/create-products-dto';
 
@@ -9,12 +9,12 @@ export class ProductsController {
   constructor(private productsService: ProductsService) {}
 
   @Get('products')
-  // @UseGuards(AuthorizationGuard)
   products(): Promise<Product[]> {
     return this.productsService.listAllProducts();
   }
 
   @Post('products')
+  @UseGuards(AuthorizationGuard)
   createProducts(@Body() productsData: ICreateProductsDTO) {
     this.productsService.createProducts(productsData);
     return 'Product created';
