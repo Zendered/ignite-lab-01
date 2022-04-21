ae6d2fe3ca1f3daac6f37fe64dd0d632c8ff955b
Luiz <luizz.ggr@gmail.com>
2022-04-21 14:21:33 -0300

    refactor: changing prefix

--- _classroom/src/http/controllers/students.controller.ts
+++ _classroom/src/http/controllers/students.controller.ts
@@ -4,28 +4,28 @@ import { CurrentUser } from '../auth/current-user';
 import { EnrollmentsService } from '../services/enrollments.service';
 import { StudentsService } from '../services/students.service';
 
-@Controller('api/v1')
+@Controller('api/v1/students')
 export class StudentsController {
   constructor(
     private studentsService: StudentsService,
     private enrollmentsService: EnrollmentsService,
   ) {}
 
-  @Get('students')
+  @Get()
   @UseGuards(AuthorizationGuard)
   students() {
     return this.studentsService.listAllStudents();
+  }
+
+  @Get('me')
+  @UseGuards(AuthorizationGuard)
+  student(@CurrentUser() user) {
+    return this.studentsService.getStudentByAuthUserId(user.sub);
   }
 
   @Get(':studentId')
   @UseGuards(AuthorizationGuard)
   enrollments(@Param('studentId') studentId: string) {
     return this.enrollmentsService.listEnrollmentsByStudent(studentId);
-  }
-
-  @Get('student')
-  @UseGuards(AuthorizationGuard)
-  student(@CurrentUser() user) {
-    return this.studentsService.getStudentByAuthUserId(user.sub);
   }
 }
