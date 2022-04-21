4d9b73fc08ce69552e654db3b400f546281cfe1d
Luiz <luizz.ggr@gmail.com>
2022-04-21 14:16:17 -0300

    feat: chack if user has that course

--- _classroom/src/http/controllers/courses.controller.ts
+++ _classroom/src/http/controllers/courses.controller.ts
@@ -1,25 +1,53 @@
-import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  UnauthorizedException,
+  UseGuards,
+} from '@nestjs/common';
 import { Course } from '@prisma/client';
+import { AuthUserDTO } from 'src/interfaces/auth-user';
 import { AuthorizationGuard } from '../auth/authorization.guard';
+import { CurrentUser } from '../auth/current-user';
 import { CoursesService } from '../services/courses.service';
+import { EnrollmentsService } from '../services/enrollments.service';
+import { StudentsService } from '../services/students.service';
 
-@Controller('api/v1')
+@Controller('api/v1/courses')
 export class CoursesController {
-  constructor(private coursesService: CoursesService) {}
+  constructor(
+    private coursesService: CoursesService,
+    private studentsService: StudentsService,
+    private enrollmentsService: EnrollmentsService,
+  ) {}
 
-  @Get('courses')
+  @Get()
   @UseGuards(AuthorizationGuard)
   courses() {
     return this.coursesService.listAllCourses();
   }
 
-  @Get(':coursesId')
+  @Get(':courseId')
   @UseGuards(AuthorizationGuard)
-  course(@Param('coursesId') coursesId) {
-    return this.coursesService.findCourseById(coursesId);
+  async course(
+    @Param('courseId') courseId: string,
+    @CurrentUser() user: AuthUserDTO,
+  ) {
+    const student = await this.studentsService.getStudentByAuthUserId(user.sub);
+    const enrollment = await this.enrollmentsService.getByCourseAndStudentId({
+      courseId,
+      studentId: student.id,
+    });
+    if (!enrollment) {
+      throw new UnauthorizedException();
+    }
+
+    return await this.coursesService.findCourseById(courseId);
   }
 
-  @Post('courses')
+  @Post()
   @UseGuards(AuthorizationGuard)
   async createCourse(@Body() data: Course) {
     return await this.coursesService.createCourse(data);
--- _classroom/src/http/services/enrollments.service.ts
+++ _classroom/src/http/services/enrollments.service.ts
@@ -1,9 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/database/prisma/prisma.service';
+import { GetByCoursesAndStudentIdParam } from 'src/interfaces/courseid-studentid';
 
 @Injectable()
 export class EnrollmentsService {
   constructor(private prisma: PrismaService) {}
+
+  async getByCourseAndStudentId({
+    courseId,
+    studentId,
+  }: GetByCoursesAndStudentIdParam) {
+    const student = await this.prisma.enrollment.findFirst({
+      where: {
+        courseId,
+        studentId,
+        canceledAt: null,
+      },
+    });
+
+    if (!student) {
+      throw new HttpException('Student not found!', HttpStatus.NOT_FOUND);
+    }
+
+    return student;
+  }
 
   listAllEnrollments() {
     return this.prisma.enrollment.findMany({
