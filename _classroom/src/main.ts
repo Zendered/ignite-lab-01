b3a0166a35cd72ed47894b363aaec644e8c34c17
Luiz <luizz.ggr@gmail.com>
2022-04-22 18:35:55 -0300

    feat: kafka config

--- _classroom/src/main.ts
+++ _classroom/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        clientId: 'classroom',
+        brokers: [`${process.env.KAFKA_BROKERS}`],
+      },
+    },
+  });
+  app.startAllMicroservices().then(() => {
+    console.log('[Classroom], Microservice running!');
+  });
   app.enableCors();
   await app.listen(3002);
 }
