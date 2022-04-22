9e04ab8b07e792f98bc95ef8eba371fcd9fd4ab8
Luiz <luizz.ggr@gmail.com>
2022-04-22 17:05:49 -0300

    feat: create a kafka message when creating a new purchase

--- purchases/src/services/purchases.service.ts
+++ purchases/src/services/purchases.service.ts
@@ -1,8 +1,9 @@
+import { KafkaService } from '@/messaging/kafka.service';
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/database/prisma/prisma.service';
 @Injectable()
 export class PurchasesService {
-  constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService, private kafka: KafkaService) {}
 
   listAllCustomer(customerId: string) {
     return this.prisma.purchase.findMany({
@@ -46,11 +47,28 @@ export class PurchasesService {
       throw new HttpException('Product not found!', HttpStatus.BAD_REQUEST);
     }
 
-    await this.prisma.purchase.create({
+    const purchase = await this.prisma.purchase.create({
       data: {
         customerId,
         productId,
       },
     });
+
+    const customer = await this.prisma.customer.findUnique({
+      where: { id: customerId },
+    });
+
+    this.kafka.emit('purchases.new-purchase', {
+      customer: {
+        authUserId: customer.authUserId,
+      },
+      product: {
+        id: product.id,
+        title: product.title,
+        slug: product.slug,
+      },
+    });
+
+    return purchase;
   }
 }
